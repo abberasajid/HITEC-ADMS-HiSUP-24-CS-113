87bc943906746a286d50cb577b8b4d6ed6a0c11a
ahmedkashi1260-ship-it <ahmedkashi1260@gmail.com>
2026-06-22 23:05:28 +0500

    [Fix] Add Program model class

---
+++ src/HiSUP/Models/Program.cs
@@ -0,0 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HiSUP.Models;
+
+public class Program
+{
+    [Key]
+    public int ProgramID { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    public string ProgramName { get; set; } = "";
+
+    [Required]
+    [StringLength(20)]
+    public string ProgramCode { get; set; } = "";
+
+    public int DepartmentID { get; set; }
+
+    [StringLength(20)]
+    public string? DegreeLevel { get; set; }
+
+    public int? DurationYears { get; set; }
+
+    public int? TotalCreditHours { get; set; }
+
+    public bool IsActive { get; set; } = true;
+
+    public DateTime? CreatedAt { get; set; }
+}
